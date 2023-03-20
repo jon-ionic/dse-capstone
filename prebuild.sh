0a559752b427de334c10dd495f30319bb71f437e
Jon <jon.herman@ionic.io>
2023-03-20 09:35:29 -0400

    add prebuild script

---
+++ prebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "prebuild"
+
+if [["$CI_PLATFORM" == "ios"]]; then
+  brew install jq gettext
+elif [["$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web"]]; then
+  apt-get install jq gettext
+fi
