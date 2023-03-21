2a3fc200ac6973f72328dd799129f2d93cd7bd0e
Jon <jon.herman@ionic.io>
2023-03-21 11:07:57 -0400

    add npmrc

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "prebuild"
-
 if [[ "$CI_PLATFORM" == "ios" ]]; then
   HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
