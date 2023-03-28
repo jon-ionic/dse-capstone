42d20ab765bf82093e68171747a7085431daccc2
Jon <jon.herman@ionic.io>
2023-03-28 12:38:09 -0400

    fix runtime command again

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 
 if [[ "$CI_PLATFORM" == "ios" ]]; then
   HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext
-  xcrun simctl runtime list
+  xcrun simctl list
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y jq gettext
 fi
