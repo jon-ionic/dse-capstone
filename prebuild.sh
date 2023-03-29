a84ae5dbfab7f5ddf7ba7e18fdc3f6317755fcf3
Jon <jon.herman@ionic.io>
2023-03-29 12:51:59 -0400

    add free command

--- prebuild.sh
+++ prebuild.sh
@@ -4,5 +4,6 @@ if [[ "$CI_PLATFORM" == "ios" ]]; then
   HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext
   xcrun simctl list
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
+  free
   apt-get update && apt-get install -y jq gettext
 fi
