ae6ae89ec79b2f8fa87154efba2109ca4b1f42ed
Jon <jon.herman@ionic.io>
2023-04-14 08:17:50 -0400

    echo python version

--- prebuild.sh
+++ prebuild.sh
@@ -4,6 +4,15 @@ if [[ "$CI_PLATFORM" == "ios" ]]; then
   echo "getting jq and gettext..."
   HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext
 
+  echo "finding python version"
+  python3 --version
+
+  echo "finding ionic cli..."
+  which ionic
+
+  echo "finding cordova cli..."
+  which cordova
+
   echo "done with prebuild tasks"
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   echo "DEBUG from prebuild: $DEBUG"
