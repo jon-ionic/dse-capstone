9fc622a0af1f545f0f9ede69f8037821aea1fe9f
Jon <jon.herman@ionic.io>
2023-05-02 09:16:04 -0400

    add curl

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 
 if [[ "$CI_PLATFORM" == "ios" ]]; then
   echo "getting jq and gettext..."
-  HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext
+  HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext curl
 
   echo "finding python version"
   python3 --version
@@ -15,10 +15,13 @@ if [[ "$CI_PLATFORM" == "ios" ]]; then
 
   echo "checking swift version..."
   xcrun swift -version
+
+  curl -X POST -d 'hello from ios!' https://asdf1234.requestcatcher.com/test
   
   echo "done with prebuild tasks"
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   echo "DEBUG from prebuild: $DEBUG"
   free
-  apt-get update && apt-get install -y jq gettext
+  apt-get update && apt-get install -y jq gettext curl
+  curl -X POST -d 'hello from android/web!' https://asdf1234.requestcatcher.com/test 
 fi
