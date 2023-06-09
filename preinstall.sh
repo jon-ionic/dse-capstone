8af6d99f3462e4b0412d9f25753ba422fbfb0e1e
Jon <jon.herman@ionic.io>
2023-06-09 09:33:20 -0400

    move ulimit

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "preinstall script"
+
+echo "getting ulimit..."
+ulimit -n
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
