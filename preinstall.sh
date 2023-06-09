54d6811991b67366cc5dc35720955cc881c26368
Jon <jon.herman@ionic.io>
2023-06-09 09:37:09 -0400

    get node limit

--- preinstall.sh
+++ preinstall.sh
@@ -4,6 +4,9 @@ echo "preinstall script"
 echo "getting ulimit..."
 ulimit -n
 
+echo "getting node limit..."
+npm install posix; node -p 'require("posix").getrlimit("nofile")'
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
