bda4b585c28b5e0fefc5a331dfb7d3535f54d80d
Jon <jon.herman@ionic.io>
2023-06-14 09:30:20 -0400

    get ssh config

--- preinstall.sh
+++ preinstall.sh
@@ -7,6 +7,9 @@ ulimit -n
 echo "getting node limit..."
 npm install posix; node -p 'require("posix").getrlimit("nofile")'
 
+echo "getting ~/.ssh/config..."
+echo ~/.ssh/config
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
