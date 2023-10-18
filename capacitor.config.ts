5ce9c2bf9d7d17ec7814a8e6712a3062844f68d8
Jon <jon.herman@ionic.io>
2023-10-18 10:34:21 -0400

    use cap live update plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,14 @@ const config: CapacitorConfig = {
   appName: 'capstone',
   webDir: 'build',
   bundledWebRuntime: false,
+  plugins: {
+    LiveUpdates: {
+      appId: '7beb099c',
+      channel: 'prod-0.0.1',
+      autoUpdateMethod: 'none',
+      maxVersions: 2,
+    }
+  }
 };
 
 export default config;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -11,59 +11,34 @@ import {
   IonCardTitle,
 } from '@ionic/react';
 import { useState } from 'react';
-import { Deploy } from 'cordova-plugin-ionic';
+import { sync, reload } from '@capacitor/live-updates';
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
-  const [updateAvailable, setUpdateAvailable] = useState({available: false, id: ''});
-  const [downloadProgress, setDownloadProgress] = useState({ progress: 0, error: ''})
-  const [extractProgress, setExtractProgress] = useState({ progress: 0, error: ''})
+const Tab1: React.FC = () => {  
+  const [update, setUpdate] = useState('')
   const [reloaded, setReloaded] = useState(false);
 
   const version = 1;
 
-  const check = async () => {
+  const syncLiveUpdate = async () => {
     try {
       setReloaded(false);
-      const update = await Deploy.checkForUpdate();
-      if (update?.available && update?.snapshot) setUpdateAvailable({available: true, id: update.snapshot});
-    } catch (e) {
-      console.log(e);
-    }
-  }
+      const availableUpdate = await sync()
+      console.log(availableUpdate)
 
-  const download = async () => {
-    try {
-      setReloaded(false);
-      if (updateAvailable.available) {
-        await Deploy.downloadUpdate((prog) => setDownloadProgress({ progress: prog || 0, error: ''}));
-        console.log(downloadProgress);
-        return;
+      if (availableUpdate) {
+        setUpdate(JSON.stringify(availableUpdate, null, 2))
       }
-      setDownloadProgress({ progress: 0, error: 'No update available.'});
     } catch (e) {
       console.log(e)
     }
   }
 
-  const extract = async () => {
+  const reloadApp = async () => {
     try {
-      setReloaded(false);
-      if (updateAvailable.available) {
-        await Deploy.extractUpdate((prog) => setExtractProgress({ progress: prog || 0, error: ''}));
-        console.log(extractProgress);
-        return;
-      }
-      setExtractProgress({ progress: 0, error: 'No update downloaded.'});
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  const reload = async () => {
-    try {
-      await Deploy.reloadApp();
+      await reload();
       setReloaded(true);
+      setUpdate('')
     } catch (e) {
       console.log('Failed to reload app.');
     }
@@ -85,38 +60,12 @@ const Tab1: React.FC = () => {
 
         <IonCard>
           <IonCardHeader>
-            <IonCardTitle>Check for Updates</IonCardTitle>
+            <IonCardTitle>Sync</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>          
-            { updateAvailable?.available && <div style={{ display: 'flex', justifyContent: 'center'}}>{updateAvailable.id} is available!</div> }
-            <IonButton onClick={(e) => check()} style={{ display: 'flex', justifyContent: 'center'}}>
-              Check
-            </IonButton>
-          </IonCardContent>
-        </IonCard>
-
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Download Update</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>          
-            { downloadProgress?.progress > 0 && <div style={{ display: 'flex', justifyContent: 'center'}}>{downloadProgress.progress}% downloaded.</div> }
-            { downloadProgress?.error && <div style={{ display: 'flex', justifyContent: 'center'}}>{downloadProgress.error}</div> }
-            <IonButton onClick={() => download()} style={{ display: 'flex', justifyContent: 'center'}}>
-              Download
-            </IonButton>
-          </IonCardContent>
-        </IonCard>
-
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Extract Update</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>          
-            { extractProgress?.progress > 0 && <div style={{ display: 'flex', justifyContent: 'center'}}>{extractProgress.progress}% extracted.</div> }
-            { extractProgress?.error && <div style={{ display: 'flex', justifyContent: 'center'}}>{extractProgress.error}</div> }
-            <IonButton onClick={() => extract()} style={{ display: 'flex', justifyContent: 'center'}}>
-              Extract
+            { update && <div style={{ display: 'flex', justifyContent: 'center'}}><pre>{update}</pre></div> }
+            <IonButton onClick={(e) => syncLiveUpdate()} style={{ display: 'flex', justifyContent: 'center'}}>
+              Sync 
             </IonButton>
           </IonCardContent>
         </IonCard>
@@ -127,7 +76,7 @@ const Tab1: React.FC = () => {
           </IonCardHeader>
           <IonCardContent>
             { reloaded && <div style={{ display: 'flex', justifyContent: 'center'}}>Reloaded.</div> }
-            <IonButton onClick={() => reload()} style={{ display: 'flex', justifyContent: 'center'}}>
+            <IonButton onClick={() => reloadApp()} style={{ display: 'flex', justifyContent: 'center'}}>
               Reload
             </IonButton>
           </IonCardContent>
