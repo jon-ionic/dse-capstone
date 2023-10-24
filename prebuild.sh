0f8e01612eac8f80f2d31851557f248a5be00b86
Jon <jon.herman@ionic.io>
2023-10-24 13:06:04 -0400

    send curl request from build phase

--- prebuild.sh
+++ prebuild.sh
@@ -15,13 +15,18 @@ if [[ "$CI_PLATFORM" == "ios" ]]; then
 
   echo "checking swift version..."
   xcrun swift -version
-
-  curl -X POST -d 'hello from ios!' https://asdf1234.requestcatcher.com/test
-  
-  echo "done with prebuild tasks"
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
-  echo "DEBUG from prebuild: $DEBUG"
-  free
   apt-get update && apt-get install -y jq gettext curl
-  curl -X POST -d 'hello from android/web!' https://asdf1234.requestcatcher.com/test 
+
+  echo "finding python version"
+  python3 --version
+
+  echo "finding ionic cli..."
+  which ionic
+
+  echo "finding cordova cli..."
+  which cordova
 fi
+
+curl -X POST -d "hello from $CI_PLATFORM!" https://jon-test.requestcatcher.com/test
+echo "done with prebuild tasks"
