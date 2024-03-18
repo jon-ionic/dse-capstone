21feec4b6ccae1c9948a835845540e40fc7b23fa
Jon <jon.herman@ionic.io>
2024-03-18 12:40:08 -0400

    show commit message

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "preinstall script"
+echo "Commit message: $CI_GIT_COMMIT_MSG"
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
