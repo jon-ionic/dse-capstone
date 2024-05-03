3e851fb7deb11264eb27ade1acb79c47202fa981
Jon <jon.herman@ionic.io>
2024-05-03 11:50:15 -0400

    rename webdir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.jon',
   appName: 'capstone',
-  webDir: 'build',
+  webDir: 'src',
   bundledWebRuntime: false,
 };
 
