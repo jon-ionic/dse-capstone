b2c2127a438e6e178d989de10b50b73fd9b31057
Jon <jon.herman@ionic.io>
2024-09-18 09:35:20 -0400

    install portals cli

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,10 @@
 echo "preinstall script"
 echo "Commit message: $CI_GIT_COMMIT_MSG"
 
+curl -sL https://raw.githubusercontent.com/ionic-team/portals-cli-releases/main/install.sh | bash
+
+portals --help
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
