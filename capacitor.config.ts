a142db52eb3f1a9eb79016f46431e518a0711855
jon-ionic <jon.herman@outsystems.com>
2024-11-05 11:36:45 -0500

    add .env hook

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import 'dotenv/config';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.jon',
   appName: 'capstone',
   webDir: 'build',
   bundledWebRuntime: false,
+  appendUserAgent: process.env?.REACT_APP_RELEASE_VERSION || '',
 };
 
 export default config;
