b35b49de2fe2d44bf7f49eaafb8bdfd3e285f3e0
jon-ionic <jon.herman@outsystems.com>
2024-11-19 15:52:37 -0500

    get ios sdks on prebuild

--- prebuild.sh
+++ prebuild.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
 if [[ "$CI_PLATFORM" == "ios" ]]; then
-  echo "getting jq and gettext..."
-  HOMEBREW_NO_AUTO_UPDATE=1 brew install jq gettext curl
-
-  echo "finding python version"
-  python3 --version
-
-  echo "finding ionic cli..."
-  which ionic
-
-  echo "finding cordova cli..."
-  which cordova
-
-  echo "checking swift version..."
-  xcrun swift -version
-
-  curl -X POST -d 'hello from ios!' https://asdf1234.requestcatcher.com/test
-  
-  echo "done with prebuild tasks"
+  echo "Building for iOS"
+  xcodebuild -showsdks
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
-  echo "DEBUG from prebuild: $DEBUG"
-  free
-  apt-get update && apt-get install -y jq gettext curl
-  curl -X POST -d 'hello from android/web!' https://asdf1234.requestcatcher.com/test 
+  echo "Building for Android"
 fi
