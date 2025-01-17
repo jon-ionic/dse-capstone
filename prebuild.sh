3ba7af4c7ccc7d20aa3eabfc326dc0b102371b50
jon-ionic <jon.herman@outsystems.com>
2025-01-17 16:00:23 -0500

    print all gems

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 
 if [[ "$CI_PLATFORM" == "ios" ]]; then
   echo "Building for iOS"
-  ls -la /opt/homebrew/Cellar/fastlane/2.220.0/libexec/gems/
+  gem query --local
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   echo "Building for Android"
 fi
