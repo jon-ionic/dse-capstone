8f130a9b75dffb376c3a874e623a77b6f17fef82
jon-ionic <jon.herman@outsystems.com>
2025-01-17 15:58:27 -0500

    print fastlane gems in prebuild

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 
 if [[ "$CI_PLATFORM" == "ios" ]]; then
   echo "Building for iOS"
-  xcodebuild -showsdks
+  ls -la /opt/homebrew/Cellar/fastlane/2.220.0/libexec/gems/
 elif [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   echo "Building for Android"
 fi
