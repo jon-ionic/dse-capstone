3b0fafdd361353fceefbf9bfa51d8c3a29fef734
jon-ionic <jon.herman@outsystems.com>
2025-04-08 12:02:29 -0400

    clone over ssh

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,7 @@ echo "Commit message: $CI_GIT_COMMIT_MSG"
 
 # try cloning private git repo over SSH
 echo $GIT_PRIVATE_SSH_KEY > ./git_private_key
-GIT_SSH_COMMAND="ssh -i ./git_private_key" git clone https://github.com/jon-ionic/portals-demo
+GIT_SSH_COMMAND="ssh -i ./git_private_key" git clone git@github.com:jon-ionic/portals-demo.git
 ls -la portals-demo
 
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
