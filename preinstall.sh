d0ffea449340c893f851096ddd5a6631291b1ff6
jon-ionic <jon.herman@outsystems.com>
2025-04-08 11:54:47 -0400

    try cloning private git repo over SSH

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,11 @@
 echo "preinstall script"
 echo "Commit message: $CI_GIT_COMMIT_MSG"
 
+# try cloning private git repo over SSH
+echo $GIT_PRIVATE_SSH_KEY > ./git_private_key
+GIT_SSH_COMMAND="ssh -i ./git_private_key" git clone https://github.com/jon-ionic/portals-demo
+ls -la portals-demo
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
