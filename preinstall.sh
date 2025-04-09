e4d9d9a495a236a718d5161690134887e4d08400
jon-ionic <jon.herman@outsystems.com>
2025-04-09 14:52:16 -0400

    make sure tools are available

--- preinstall.sh
+++ preinstall.sh
@@ -2,19 +2,19 @@
 echo "preinstall script"
 echo "Commit message: $CI_GIT_COMMIT_MSG"
 
-echo "> curl -I https://repository.meinestadt.de/artifactory/api/npm/meinestadt-npm/"
+echo "> curl --max-time 10 -I https://repository.meinestadt.de/artifactory/api/npm/meinestadt-npm/"
 curl -I https://repository.meinestadt.de/artifactory/api/npm/meinestadt-npm/
-
-echo "> ping repository.meinestadt.de"
-ping repository.meinestadt.de
-
-echo "> traceroute repository.meinestadt.de"
-traceroute repository.meinestadt.de
 
 echo "> npm whoami --registry=https://repository.meinestadt.de/artifactory/api/npm/meinestadt-npm/"
 npm whoami --registry=https://repository.meinestadt.de/artifactory/api/npm/meinestadt-npm/
 
 
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
-  apt-get update && apt-get install -y libpcap-dev
+  apt-get update && apt-get install -y traceroute iputils-ping
 fi
+
+echo "> ping -c 4 repository.meinestadt.de"
+ping repository.meinestadt.de
+
+echo "> traceroute repository.meinestadt.de"
+traceroute repository.meinestadt.de
