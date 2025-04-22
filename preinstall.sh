5d6cdbdbdf1739d048af07dccab4c659fc819921
jon-ionic <jon.herman@outsystems.com>
2025-04-22 15:30:32 -0400

    print ruby version
    Signed-off-by: jon-ionic <jon.herman@outsystems.com>

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,10 @@
 echo "preinstall script"
 echo "Commit message: $CI_GIT_COMMIT_MSG"
 
+if [[ "$CI_PLATFORM" == "ios" ]]; then
+  ruby -v
+fi
+
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
 fi
