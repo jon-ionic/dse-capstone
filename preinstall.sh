ec11b736b8c66a42f4a90e6a389541f62d0ad0d4
Jon Herman <jon.herman@outsystems.com>
2025-05-27 14:32:44 -0400

    print ruby version regardless of platform

--- preinstall.sh
+++ preinstall.sh
@@ -2,10 +2,9 @@
 echo "preinstall script"
 echo "Commit message: $CI_GIT_COMMIT_MSG"
 
-if [[ "$CI_PLATFORM" == "ios" ]]; then
-  echo "Ruby version: $(ruby -v)"
-  echo "Gem version: $(gem -v)"
-fi
+echo "Ruby version: $(ruby -v)"
+echo "Gem version: $(gem -v)"
+
 
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
