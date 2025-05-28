6f1c792d5767a0a8a8a098c3926daeeda5042049
Jon Herman <jon.herman@outsystems.com>
2025-05-28 12:12:26 -0400

    upgrade ruby during build

--- preinstall.sh
+++ preinstall.sh
@@ -5,6 +5,12 @@ echo "Commit message: $CI_GIT_COMMIT_MSG"
 echo "Ruby version: $(ruby -v)"
 echo "Gem version: $(gem -v)"
 
+if [[ "$CI_PLATFORM" == "ios" ]]; then
+  brew install ruby@3.4
+  echo "Ruby location: $(which ruby)"
+  echo "Ruby version: $(ruby -v)"
+fi
+
 
 if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
   apt-get update && apt-get install -y libpcap-dev
