4bdd774b78d176f1f377ff8fd7da3dad2889598e
Jon Herman <jon.herman@outsystems.com>
2025-12-09 13:21:53 -0800

    remove libpcap-dev

--- preinstall.sh
+++ preinstall.sh
@@ -4,8 +4,3 @@ echo "Commit message: $CI_GIT_COMMIT_MSG"
 
 echo "Ruby version: $(ruby -v)"
 echo "Gem version: $(gem -v)"
-
-
-if [[ "$CI_PLATFORM" == "android" || "$CI_PLATFORM" == "web" ]]; then
-  apt-get update && apt-get install -y libpcap-dev
-fi
